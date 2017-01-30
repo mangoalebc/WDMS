b42f6dcfc6bcbae00041997e1fca35cea8c4cbaa
mangoalebc <bokangmangoale@gmail.com>
2017-01-30 11:43:36 +0200

    Add files via upload

--- Home.php
+++ Home.php
@@ -1,7 +1,3 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
@@ -11,45 +7,111 @@ and open the template in the editor.
     </head>
     <body>
         <div class ="container">
-            <div class ="row main">
-                <div class ="main-login main-center">
-                    <h2>Activities</h2>
-                    <form name="login" action="http://146.64.213.194/Bokang/Test6.php" method="POST">
+            <div class ="main-center" style ="padding: 10px 50px;">
+               <h2>Activities</h2>
+            </div>
+            
+            <div class ="main-login" style = "background:#009edf; margin: 0 auto; max-width: 900px; padding: 10px 40x;color: #fff; text-shadow: none;  margin-top: 100px; ">
 
-
-                     <div class ="form-group">
-                         <input type="submit" value="Check" name="Check" id="button" class="btn btn-primary btn-lg btn-block login-button" onclick="<a href='http://146.64.213.194/Bokang/API/Test6.php'>Check</a>" />
-                    </div>
-
-                    </form>
-
-                    <form name="login" action="http://146.64.213.194/Bokang/API/Logout" method="POST">
+                <form name="login" action="http://146.64.204.58/Bokang/API/Logout" method="POST">
 
                     <div class ="form-group">
-                          <input type="submit" value="Logout" name="Logout" id="button" class="btn btn-primary btn-lg btn-block login-button" />
-                    </div>
-                    </form>
 
-                </div>
-            </div>
-        </div>
-        <?php
-        // put your code here
+                        <table style = "border: hidden 3px #fff; width: 100%" class="form-control">
+                            <thead>
+                              <tr style = "color: blue ; width: 100%">
+                                    <th >#</th>
+                                    <th>FileName</th>
+                                    <th>Last Checked</th>
+                                    <th>Last Replaced</th>
+                                    <th>FileStatus</th>
+                                    <th>Action</th>
+                              </tr>
 
+                            <?php
+                            $FileID= "";
+                            $servername = "127.0.0.1";
+                            $username = "root";
+                            $password = "";
+                            $dbname = "academycity";
 
-echo "<table style  = 'border: solid 1px black;' class = 'form-group'>";
-echo "<tr class ='form-control'><th>ID</th><th>Email </th><th>Name</th><th>More Info</th></tr>";
+                            // Create connection
+                            $conn = new mysqli($servername, $username, $password, $dbname);
+                            // Check connection
+                            if ($conn->connect_error) {
+                                    die("Connection failed: " . $conn->connect_error);
+                            }
+                            
+  
+                            $conn2 = new mysqli($servername, $username, $password, $dbname);
+                            $query2 ="SELECT * FROM Files";
+                            $result = mysqli_query($conn2,$query2);
+                            while ($row = mysqli_fetch_row($result)){
+                                //var_dump($row[2])."\r\n";
+                               //taking data from database to views the form table
+                              $FileID=$row[0];
+                              $colour=$row[4];
+                              echo "<tr class ='form-control' style = 'color:#fff'>";
+                              echo"<th>";
+                              echo $row[0];
+                              echo"</th><th>";
 
+                              echo $row[1];
+                              echo"</th><th>";
+                              echo $row[2];
+                              echo"</th><th>";
+                              echo $row[3];
+                              echo"</th><th>";
+                              echo $colour;
+                              echo"</th><th>";
+                              echo"<a href=moreinfo.php?UserFileID=$FileID>More Info</a>";
+                              echo "</th></tr>";
 
+                            //var table = $(".table");
+                                    //var $tr = $("tr").append(
+                                            //$("<td>").html(getColour("green"))
+                                    //);
+                                    //$tr.appendTo(".table > tbody");
+                                                                                      }
+                            $conn->close();
+                            ?>
 
+                    <script>
+                    // put your code here
+                    /********************************************************
+                       *    Utility functions for datatable column display     *
+                       ********************************************************/
+                       function getColour(val)
+                       {
+                                    // Do not draw the rectangle if indicator does not have a tlp marking/tag
+                                    if(val != "red" && val != "green" && val != "white" && val != "amber") return "";
+                                    var svg1 = "<svg width=\"20\" height=\"20\"><circle cx=\"10\" cy=\"10\" r=\"9\" width=\"35\" height=\"26\" style=\'fill:";
+                                    var svg2 = ";\stroke:black;stroke-width:1.2;opacity:0.6\'></circle></svg>"
+                                    if(val.toLowerCase() == "amber") return svg1 + "yellow" + svg2;
+                                    else
+                                            return svg1 + val + svg2;
+                       }
+                    var table = $(".table");
+                            var $tr = $("tr").append(
+                                    $("<td>").html(getColour("green"))
+                            );
+                            $tr.appendTo(".table > tbody");
 
-echo "</table>";
-echo "<br/>";
-echo "<br/>";
-echo "<br/>";
+                    </script>
 
+               </thead>
+             </tbody>
+            </table>
 
+          </div>
 
-        ?>
+          <div class ="form-group">
+                <input type="submit" value="Logout" name="Logout" id="button" class="btn btn-primary btn-lg btn-block login-button" />
+          </div>
+       </form>
+
+      </div>
+    </div>
+          
     </body>
 </html>
