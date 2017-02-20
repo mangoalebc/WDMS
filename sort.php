3b3ec7fed0d323346df5943a8b55dd7025a5344f
mangoalebc <bokangmangoale@gmail.com>
2017-02-20 02:15:17 -0800

    Add files via upload

---
+++ sort.php
@@ -0,0 +1,39 @@
+<!--
+  ** Style a Select Box Using Only CSS
+  ** Source: http://bavotasan.com/2011/style-select-box-using-only-css/
+  ** Source: http://danielneumann.com/blog/how-to-style-dropdown-with-css-only/
+  ** Source: http://stackoverflow.com/a/5809186
+-->
+<html>
+<head>
+	<link rel="stylesheet" href="select.css">
+</head>
+<body>
+
+
+<div class="styled-select green rounded">
+  <select>
+    <option>Here is the first option</option>
+    <option>The second option</option>
+    <option>The third option</option>
+  </select>
+</div>
+
+<div class="styled-select blue semi-square">
+  <select>
+    <option>Here is the first option</option>
+    <option>The second option</option>
+    <option>The third option</option>
+  </select>
+</div>
+
+<div class="styled-select yellow rounded">
+  <select>
+    <option>Here is the first option</option>
+    <option>The second option</option>
+    <option>The third option</option>
+  </select>
+</div>
+
+</body>
+</html>
